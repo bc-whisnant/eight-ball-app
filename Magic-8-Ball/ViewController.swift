305c35406f0b69fde5d5779a3e48da92a5cd99ce
bc-whisnant <bc.whisnant@gmail.com>
2018-10-10 21:06:25 -0400

    Initial Commit

---
+++ Magic-8-Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic-8-Ball
+//
+//  Created by Galvatron on 10/10/18.
+//  Copyright © 2018 Galvatron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
